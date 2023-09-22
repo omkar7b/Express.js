39a34c9b406b49758e7e83f18e8a5106962ef329
Omkar Bende <136046556+omkar7b@users.noreply.github.com>
2023-09-22 12:51:56 +0530

    Add files via upload

---
+++ Retrieving Data From Database/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Omkarb@7'
+});
+
+module.exports = pool.promise();
